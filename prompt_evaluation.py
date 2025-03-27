703f0db9699be4c526e5d4ec266f870b1b84e992
Daniel Sobczynski <daniel_sobczynski@baxter.com>
2025-03-27 20:26:43 +0000

    created run eval loop in prompt_evaluation.py

---
+++ prompt_evaluation.py
@@ -0,0 +1,65 @@
+# Lets say we have a list of llm-generated SW artifacts, such as requirements
+
+def add_spaces(x: str) -> str:
+    return f" {x} "
+
+def convert_bool_to_ohe(bool_result: bool) -> int:
+    if bool_result:
+        return 1
+    else:
+        return 0
+
+def eval_has_a_def_article(text: str) -> bool:
+    """
+    R5: Criteria from 4.1.5 INCOSE Guide to Writing Requirements:
+        check if text contains indefinite article \"a\" 
+    """
+    if add_spaces("a") in text:
+        return True
+    else:
+        return False
+
+def eval_has_vague_terms(text: str) -> bool:
+    """
+    R7: Criteria from 4.1.7 INCOSE Guide to Writing Requirements:
+        check if text contains vague terms        
+    """
+    vague_terms = [
+        "some", "any", "allowable", "several", "many", "a lot of", "a few", "almost always", 
+        "very nearly", "nearly", "about", "close to", "almost","approximate","ancillary", "relevant", 
+        "routine", "common", "generic", "significant","flexible", "expandable", "typical", "sufficient", 
+        "adequate", "appropriate", "efficient", "effective", "proficient", "reasonable","customary",
+        "usually", "approximately", "sufficiently","typically"
+    ]
+    for term in vague_terms:
+        if add_spaces(term) in text:
+            return True
+    else:
+        return False
+
+def eval_has_escape_clauses(text:str) -> bool:
+    pass
+
+eval_funcs = {
+    'eval_has_a_def_article':eval_has_a_def_article,
+    'eval_has_vague_terms': eval_has_vague_terms,
+    'eval_has_escape_clauses': eval_has_escape_clauses,
+}
+
+llm_results = [
+    'This is a result',
+    'This is another result',
+]
+result_col = "llm_results"
+df = pd.DataFrame({result_col:llm_results})
+
+# run evals for each row of the dataframe
+for _index, _row in df.iterrows():
+    for key, value in eval_funcs.items(): 
+        eval_func_to_call = eval_funcs[key] 
+        eval_result = eval_func_to_call(_row[result_col])
+        df.loc[_index, key] = convert_bool_to_ohe(
+            eval_result
+        )
+print(df.head(5))
+# get accuracy scores for each eval function
